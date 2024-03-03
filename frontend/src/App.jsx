b310d2dcb691a818ffc5573ee956b9faed7cfada
Csörgő Csaba <csabi16sheldon@gmail.com>
2024-03-03 18:44:53 +0100

    added basic login verification

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,32 @@
 import { useState, useEffect } from "react";
 import "./App.css";
 import axios from "axios";
+import Loading from "./components/Loading";
 
 function App() {
-  
   const [authorizationCode, setAuthorizationCode] = useState("");
   const [accessToken, setAccessToken] = useState("");
   const [userData, setUserData] = useState("");
+  const [loginError, setLoginError] = useState(false);
+  const [loading, setLoading] = useState(false);
 
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        setLoading(true);
+        const response = await axios.post("http://localhost:3000/auth/verifyLogin", userData.data);
+        if (!response.data.allowed && !response.data.loading) {
+          logout(response.data.message);
+        }
+      } catch (error) {
+        console.error("Error:", error);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchData();
+  }, [userData]);
 
   useEffect(() => {
     const getStoredToken = () => {
@@ -23,12 +42,12 @@ function App() {
 
   useEffect(() => {
     fetchUserData();
-  }, [accessToken])
-  
+    console.log(accessToken);
+  }, [accessToken]);
 
   function getToken(code) {
     axios
-      .post("http://localhost:3000/getToken", { code })
+      .post("http://localhost:3000/auth/getToken", { code })
       .then((response) => {
         const token = response.data.access_token;
         setAccessToken(token);
@@ -59,12 +78,16 @@ function App() {
     window.location.href = "https://auth-dev.vatsim.net/oauth/authorize?client_id=745&response_type=code&scope=full_name+email+vatsim_details&redirect_uri=http://localhost:5173";
   }
 
-  function logout() {
+  function logout(err = false) {
     localStorage.removeItem("accessToken");
     setAccessToken("");
-    setAuthorizationCode(""); // Reset authorizationCode state to empty string
-    setUserData(""); // Reset authorizationCode state to empty string
+    setAuthorizationCode("");
+    setUserData("");
+    if (err) {
+      setLoginError(err);
+    }
   }
+  
 
   function fetchUserData() {
     if (accessToken) {
@@ -81,7 +104,6 @@ function App() {
       axios(config)
         .then((response) => {
           setUserData(response.data);
-          console.log(response.data)
         })
         .catch((error) => {
           console.log(error);
@@ -96,12 +118,17 @@ function App() {
       {authorizationCode === "authorized" ? (
         <div>
           <p>Authorization Code: {authorizationCode}</p>
-          {userData ? userData.data.cid : "no user data"}
-          <button onClick={logout}>Log out</button>
-          
+          <>
+            {userData ? userData.data.cid : "no user data"}
+            <button onClick={() => logout()}>Log out</button>
+
+          </>
         </div>
       ) : (
-        <button onClick={login}>Login</button>
+        <>
+          <p>{loginError ? loginError : ""}</p>
+          <button onClick={login}>Login</button>
+        </>
       )}
     </>
   );
