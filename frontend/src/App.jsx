679b947d34cdd8274464fb2e5a3bd3b03a73baff
Csörgő Csaba <csabi16sheldon@gmail.com>
2024-03-04 18:03:31 +0100

    login route

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,35 @@
 import React from 'react'
+import { useState, useEffect } from 'react';
+import { useNavigate } from "react-router-dom";
+import Loading from './components/Loading';
 
 export default function App() {
+  const navigate = useNavigate();
+  const [accessToken, setAccessToken] = useState(false)
+  useEffect(() => {
+    const getStoredToken = () => {
+      const storedToken = localStorage.getItem("accessToken");
+      if (storedToken) {
+        setAccessToken(storedToken);
+      } else {
+        console.log("redirecting....")
+        navigate("/login")
+      }
+    };
+
+    getStoredToken();
+  }, []);
+
+  function logout() {
+    localStorage.removeItem("accessToken");
+    setAccessToken("");
+    navigate("/login");
+  }
+
   return (
-    <div>App</div>
+    <div>
+      <Loading></Loading>
+      <button onClick={logout}>Logout</button>
+    </div>
   )
 }
