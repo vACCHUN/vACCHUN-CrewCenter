4399c3c62ce033adf7fe4cc833ad2aa202b55d81
Csaba44 <csabi16sheldon@gmail.com>
2024-04-04 16:00:27 +0200

    did nothing basically

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -9,7 +9,22 @@ export default function App() {
   const navigate = useNavigate();
   const [accessToken, setAccessToken] = useState("");
   const [userData, setUserData] = useState("");
+  const [bookingData, setBookingData] = useState("");
   const [loginValid, setLoginValid] = useState("");
+
+  useEffect(() => {
+    const fetchBookingData = async () => {
+      try {
+        const response = await axios.get("http://localhost:3000/bookings");
+
+        setBookingData(response.data.Bookings);
+      } catch (error) {
+        console.error("Error:", error);
+      } finally {
+      }
+    };
+    fetchBookingData();
+  }, []);
 
   useEffect(() => {
     function fetchUserData() {
@@ -90,8 +105,48 @@ export default function App() {
     <>
       {loginValid ? (
         <div className="pt-[30px]">
-          <Nav/>
-          <button className="bg-white hover:bg-gray-200 text-gray-800 font-semibold py-2 px-4 border border-gray-400 rounded shadow" onClick={logout}>Logout</button>
+          <Nav />
+          <div className="border-b-2 border-black font-bold pl-7">Beültetés ATS 2023. 11. 23.</div>
+          <table className="text-center">
+            <thead className="sticky">
+              <tr className="headerrow">
+                <td rowSpan={2}>Helyi idő</td>
+                <td>BTWR</td>
+                <td>SV</td>
+                <td>ADC</td>
+                <td>GRC</td>
+                <td>TPC</td>
+                <td>CDC</td>
+              </tr>
+              <tr className="headerrow align-top">
+                <td>BTWR</td>
+                <td>SV</td>
+                <td>ADC</td>
+                <td>GRC</td>
+                <td>TPC</td>
+                <td>CDC</td>
+              </tr>
+            </thead>
+            <tbody>
+              {(() => {
+                const rows = [];
+                for (let hour = 0; hour < 24; hour++) {
+                  rows.push(
+                    <tr className="timerow" key={hour}>
+                      <td className="">{`${hour}:00 - ${hour + 1}:00`}</td>
+                      {hour == 2 ? <td>CS</td> : <td></td>}
+                      <td></td>
+                      <td></td>
+                      <td></td>
+                      <td></td>
+                      <td></td>
+                    </tr>
+                  );
+                }
+                return rows;
+              })()}
+            </tbody>
+          </table>
         </div>
       ) : (
         <Loading message="Verifying login..." />
