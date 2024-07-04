eae4d4571acefdaad10db1273837895441a68c8e
Csaba44 <csabi16sheldon@gmail.com>
2024-07-04 13:52:53 +0200

    added booking table, date selector, gets data from the DB

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,26 +1,25 @@
-import React from "react";
-import { useState, useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import Loading from "./components/Loading";
 import axios from "axios";
 import Nav from "./components/Nav";
+import BookingTable from "./components/BookingTable";
 
 export default function App() {
   const navigate = useNavigate();
   const [accessToken, setAccessToken] = useState("");
   const [userData, setUserData] = useState("");
-  const [bookingData, setBookingData] = useState("");
-  const [loginValid, setLoginValid] = useState("");
+  const [bookingData, setBookingData] = useState([]);
+  const [loginValid, setLoginValid] = useState(false);
+  const [selectedDate, setSelectedDate] = useState("");
 
   useEffect(() => {
     const fetchBookingData = async () => {
       try {
         const response = await axios.get("http://localhost:3000/bookings");
-
         setBookingData(response.data.Bookings);
       } catch (error) {
         console.error("Error:", error);
-      } finally {
       }
     };
     fetchBookingData();
@@ -32,7 +31,7 @@ export default function App() {
         let config = {
           method: "get",
           maxBodyLength: Infinity,
-          url: "https://auth-dev.vatsim.net/api/user?client_id=745",
+          url: "https://auth-dev.vatsim.net/api/user?client_id=1389",
           headers: {
             Accept: "application/json",
             Authorization: `Bearer ${accessToken}`,
@@ -56,7 +55,7 @@ export default function App() {
 
   useEffect(() => {
     if (userData) {
-      if (userData.oauth.token_valid == "false") {
+      if (userData.oauth.token_valid === "false") {
         logout();
       }
 
@@ -72,7 +71,6 @@ export default function App() {
           }
         } catch (error) {
           console.error("Error:", error);
-        } finally {
         }
       };
 
@@ -94,6 +92,12 @@ export default function App() {
     getStoredToken();
   }, []);
 
+  useEffect(() => {
+    const today = new Date();
+    const formattedToday = today.toISOString().split('T')[0];
+    setSelectedDate(formattedToday);
+  }, []);
+
   function logout(err) {
     const errorMessage = err && typeof err === "string" ? err : "You have been logged out";
     localStorage.removeItem("accessToken");
@@ -101,52 +105,19 @@ export default function App() {
     navigate("/login", { state: { errorMessage } });
   }
 
+  const handleDateChange = (event) => {
+    setSelectedDate(event.target.value);
+  };
+
   return (
     <>
       {loginValid ? (
         <div className="pt-[30px]">
           <Nav />
-          <div className="border-b-2 border-black font-bold pl-7">Beültetés ATS 2023. 11. 23.</div>
-          <table className="text-center">
-            <thead className="sticky">
-              <tr className="headerrow">
-                <td rowSpan={2}>Helyi idő</td>
-                <td>BTWR</td>
-                <td>SV</td>
-                <td>ADC</td>
-                <td>GRC</td>
-                <td>TPC</td>
-                <td>CDC</td>
-              </tr>
-              <tr className="headerrow align-top">
-                <td>BTWR</td>
-                <td>SV</td>
-                <td>ADC</td>
-                <td>GRC</td>
-                <td>TPC</td>
-                <td>CDC</td>
-              </tr>
-            </thead>
-            <tbody>
-              {(() => {
-                const rows = [];
-                for (let hour = 0; hour < 24; hour++) {
-                  rows.push(
-                    <tr className="timerow" key={hour}>
-                      <td className="">{`${hour}:00 - ${hour + 1}:00`}</td>
-                      {hour == 2 ? <td>CS</td> : <td></td>}
-                      <td></td>
-                      <td></td>
-                      <td></td>
-                      <td></td>
-                      <td></td>
-                    </tr>
-                  );
-                }
-                return rows;
-              })()}
-            </tbody>
-          </table>
+          <div className="font-bold pl-7">
+            Beültetés ATS <input type="date" value={selectedDate} onChange={handleDateChange} />
+          </div>
+          <BookingTable bookings={bookingData} selectedDate={selectedDate} />
         </div>
       ) : (
         <Loading message="Verifying login..." />
