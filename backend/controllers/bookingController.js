4c885f4c9d302d6c6f3ead43ee04f833443bcac7
Csaba44 <csabi16sheldon@gmail.com>
2024-07-24 22:17:32 +0200

    fixed all problems with the table (hopefully), started working on training bookings

--- backend/controllers/bookingController.js
+++ backend/controllers/bookingController.js
@@ -1,10 +1,11 @@
 const con = require("../config/mysql");
 const util = require("util");
 const query = util.promisify(con.query).bind(con);
+const axios = require("axios");
 
 const getAllBookings = async () => {
   try {
-    const result = await query(`SELECT * from controllerBookings`);
+    const result = await query(`SELECT * from controllerBookings ORDER BY startTime`);
     return { Bookings: result, count: result.length };
   } catch (error) {
     return { error: error };
@@ -13,7 +14,7 @@ const getAllBookings = async () => {
 
 const getBookingsByInitial = async (initial) => {
   try {
-    const result = await query(`SELECT * from controllerBookings WHERE initial = '${initial}'`);
+    const result = await query(`SELECT * from controllerBookings WHERE initial = '${initial}' ORDER BY startTime`);
     return { Bookings: result, count: result.length };
   } catch (error) {
     return { error: error };
@@ -22,17 +23,16 @@ const getBookingsByInitial = async (initial) => {
 
 const getBookingsByDate = async (date) => {
   try {
-    const result = await query(`SELECT * from controllerBookings WHERE DATE(startTime) = '${date}'`);
+    const result = await query(`SELECT * from controllerBookings WHERE DATE(startTime) = '${date}' ORDER BY startTime`);
     return { Bookings: result, count: result.length };
   } catch (error) {
     return { error: error };
   }
 };
 
-
 const getBookingByID = async (id) => {
   try {
-    const result = await query(`SELECT * from controllerBookings WHERE id = ${id}`);
+    const result = await query(`SELECT * from controllerBookings WHERE id = ${id} ORDER BY startTime`);
     return { Bookings: result, count: result.length };
   } catch (error) {
     return { error: error };
@@ -44,10 +44,24 @@ const createBooking = async (initial, cid, name, startTime, endTime, sector, sub
     return { message: "Missing fields." };
   }
 
+  let training = 0;
+
+  try {
+    const response = await axios.get(`https://api.vatsim.net/v2/members/1582533`);
+    const userRating = response.data.rating;
+    const minRatingQ = await query(`SELECT minRating from sectors WHERE id = '${sector}'`);
+    const minRating = minRatingQ[0].minRating;
+
+    training = userRating < minRating ? 1 : 0;
+  } catch (error) {
+    return { error: error };
+  }
+
+
   try {
     const result = await query(`
-      INSERT INTO controllerBookings (initial, cid, name, startTime, endTime, sector, subSector) 
-      VALUES ('${initial}', ${cid}, '${name}', '${startTime}', '${endTime}', '${sector}', '${subSector}')
+      INSERT INTO controllerBookings (initial, cid, name, startTime, endTime, sector, subSector, training) 
+      VALUES ('${initial}', ${cid}, '${name}', '${startTime}', '${endTime}', '${sector}', '${subSector}', ${training})
     `);
     return { result: result };
   } catch (error) {
