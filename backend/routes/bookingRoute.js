44d69de4cc342d24e3e8ee87ab3cec6832a83d30
Csaba44 <csabi16sheldon@gmail.com>
2024-07-25 19:52:24 +0200

    added safety measure to not be able to insert time values not divisible by 5 minutes

--- backend/routes/bookingRoute.js
+++ backend/routes/bookingRoute.js
@@ -3,6 +3,28 @@ const router = express.Router();
 require("dotenv").config();
 const bookingController = require("../controllers/bookingController.js");
 
+
+function formatDateToMySQL(datetime) {
+  const date = new Date(datetime);
+  const year = date.getFullYear();
+  const month = String(date.getMonth() + 1).padStart(2, '0');
+  const day = String(date.getDate()).padStart(2, '0');
+  const hours = String(date.getHours()).padStart(2, '0');
+  const minutes = String(date.getMinutes()).padStart(2, '0');
+  const seconds = String(date.getSeconds()).padStart(2, '0');
+  return `${year}-${month}-${day} ${hours}:${minutes}:${seconds}`;
+}
+
+function roundTime(time) {
+  const date = new Date(time);
+  const minutes = date.getMinutes();
+  const roundedMinutes = Math.round(minutes / 5) * 5;
+  date.setMinutes(roundedMinutes);
+  date.setSeconds(0);
+  date.setMilliseconds(0);
+  return formatDateToMySQL(date);
+}
+
 router.get("/", async (req, res) => {
   try {
     const bookings = await bookingController.getAllBookings();
@@ -53,7 +75,11 @@ router.post("/add", async (req, res) => {
   }
   try {
     console.log(req.body);
-    const bookings = await bookingController.createBooking(req.body.initial, req.body.cid, req.body.name,  req.body.startTime, req.body.endTime, req.body.sector, req.body.subSector);
+    const roundedStartTime = roundTime(req.body.startTime);
+    const roundedEndTime = roundTime(req.body.endTime);
+    console.log(roundedStartTime, roundedEndTime)
+    const bookings = await bookingController.createBooking(req.body.initial, req.body.cid, req.body.name,  roundedStartTime, roundedEndTime, req.body.sector, req.body.subSector);
+    console.log(bookings)
     return res.status(200).send(bookings);
   } catch (error) {
     console.log(error.message);
