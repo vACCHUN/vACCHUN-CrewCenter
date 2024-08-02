8002d8470b2ef8662ab8274780bed9a6d224c7e1
Csaba44 <csabi16sheldon@gmail.com>
2024-08-02 13:46:04 +0000

    stopped tracking config.js

--- frontend/src/config.js
+++
@@ -1,8 +0,0 @@
-const config = {
-  API_URL: "http://cc.vacchun.hu:3000",
-  CLIENT_ID: 1389,
-  VATSIM_API_URL: "https://auth.vatsim.net",
-  VATSIM_REDIRECT: "http://cc.vacchun.hu/login"
-};
-
-export default config;
