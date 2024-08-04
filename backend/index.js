d2a2ac5ec051aaf8a45debe9ec98e703c76c5b16
Csaba44 <csabi16sheldon@gmail.com>
2024-08-04 10:33:16 +0000

    'working'

--- backend/index.js
+++ backend/index.js
@@ -7,27 +7,29 @@ const bookingRoute = require("./routes/bookingRoute.js");
 const authRoute = require("./routes/authRoute.js");
 const sectorRoute = require("./routes/sectorRoute.js");
 
-
-
 const PORT = 3000;
 
+// Trust proxy
+app.set('trust proxy', true);
+
+// CORS configuration
+const corsOptions = {
+  origin: 'https://cc.vacchun.hu', // Update this to your frontend's URL
+  optionsSuccessStatus: 200
+};
+app.use(cors(corsOptions));
 
 app.use(express.json());
-app.use(cors());
 
-
-app.use("/atcos", atcoRoute);
-app.use("/bookings", bookingRoute);
-app.use("/auth", authRoute);
-app.use("/sectors", sectorRoute);
-
+app.use("/api/atcos", atcoRoute);
+app.use("/api/bookings", bookingRoute);
+app.use("/api/auth", authRoute);
+app.use("/api/sectors", sectorRoute);
 
 app.get("/", (req, res) => {
   res.json({ message: "Express is running." });
 });
 
-
-
 app.listen(PORT, () => {
-  console.log(`http://localhost:${PORT}`);
+  console.log(`Server running at http://localhost:${PORT}`);
 });
