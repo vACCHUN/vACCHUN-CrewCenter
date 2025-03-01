e10d51a42ca289c5f95c1c9c475b91bec7af11f0
Csaba <74451429+Csaba44@users.noreply.github.com>
2025-03-01 17:42:43 +0000

    fplans still persist after disconnecting

--- backend/websocket.js
+++ backend/websocket.js
@@ -1,39 +1,24 @@
 const WebSocket = require("ws");
 const axios = require("axios");
 
-let vatsimData = []; // This will be rebuilt every time
+let vatsimData = []; 
 let currentDay = null;
 let currentMessageNumber = 0;
 
-const LHCC_BELEPOK = [
-  "ABETI", "ABONY", "ABULI", "ALAMU", "AMRAX", "ANIWE", "ARSIN", "BABIT", "BABOX", "BADOR", 
-  "BADOV", "BALAP", "BALUX", "BAREB", "BEGLA", "BETED", "BINKU", "BODZA", "BOKSI", "BUDOP", 
-  "BUZRA", "DEGET", "DEMOP", "DIMLO", "DODAR", "DUZLA", "EBAMO", "EBORO", "EDEMU", "EMBUT", 
-  "EPARI", "ERGOM", "ERGUZ", "ETARO", "ETNOG", "FAHAZ", "FOGRE", "GASNA", "GAZDA", "GELKA", 
-  "GEMTO", "GILEP", "GITAS", "GOTAR", "IBLIZ", "ILHAK", "INVED", "JOZEP", "KARIL", "KEKED", 
-  "KENIN", "KEROP", "KEZAL", "KOLUM", "KOPRY", "KOVEK", "KUSIS", "KUVEX", "LAHOR", "LATOF", 
-  "LITKU", "LONLA", "LUVEL", "MAVIR", "MEGIK", "MIZOL", "MOPUG", "NALOX", "NARKA", "NATEX", 
-  "NEKIN", "NIKAB", "NIPUR", "NOHAT", "NORAH", "OGVUN", "OKORA", "OLATI", "ONNIS", "OSDUK", 
-  "OSLEN", "PARAK", "PATAK", "PEJKO", "PERIT", "PESAT", "PIDON", "PITOK", "PUCOG", "PUSTA", 
-  "RAKFA", "RIGSA", "ROMKA", "SASAL", "SIRDU", "SOGMO", "SOPRO", "STEIN", "SUBES", "SUFAX", 
-  "SUNIS", "SUNOR", "TEGRI", "TEKNO", "TONDO", "TORNO", "ULZAK", "UVERA", "VAJDI", "VAMOG", 
-  "VAJDI", "VEBAL", "VEBOS", "VERIG", "VETIK", "WITRI", "XOMBA", "ZOLKU", "ZURFA"
-];
+const LHCC_BELEPOK = ["ABETI", "ABONY", "ABULI", "ALAMU", "AMRAX", "ANIWE", "ARSIN", "BABIT", "BABOX", "BADOR", "BADOV", "BALAP", "BALUX", "BAREB", "BEGLA", "BETED", "BINKU", "BODZA", "BOKSI", "BUDOP", "BUZRA", "DEGET", "DEMOP", "DIMLO", "DODAR", "DUZLA", "EBAMO", "EBORO", "EDEMU", "EMBUT", "EPARI", "ERGOM", "ERGUZ", "ETARO", "ETNOG", "FAHAZ", "FOGRE", "GASNA", "GAZDA", "GELKA", "GEMTO", "GILEP", "GITAS", "GOTAR", "IBLIZ", "ILHAK", "INVED", "JOZEP", "KARIL", "KEKED", "KENIN", "KEROP", "KEZAL", "KOLUM", "KOPRY", "KOVEK", "KUSIS", "KUVEX", "LAHOR", "LATOF", "LITKU", "LONLA", "LUVEL", "MAVIR", "MEGIK", "MIZOL", "MOPUG", "NALOX", "NARKA", "NATEX", "NEKIN", "NIKAB", "NIPUR", "NOHAT", "NORAH", "OGVUN", "OKORA", "OLATI", "ONNIS", "OSDUK", "OSLEN", "PARAK", "PATAK", "PEJKO", "PERIT", "PESAT", "PIDON", "PITOK", "PUCOG", "PUSTA", "RAKFA", "RIGSA", "ROMKA", "SASAL", "SIRDU", "SOGMO", "SOPRO", "STEIN", "SUBES", "SUFAX", "SUNIS", "SUNOR", "TEGRI", "TEKNO", "TONDO", "TORNO", "ULZAK", "UVERA", "VAJDI", "VAMOG", "VAJDI", "VEBAL", "VEBOS", "VERIG", "VETIK", "WITRI", "XOMBA", "ZOLKU", "ZURFA"];
 
 async function fetchVatsimData() {
   try {
     const response = await axios.get("https://data.vatsim.net/v3/vatsim-data.json");
     const dayOfMonth = new Date().getDate();
 
-    // Reset data if it's a new day
     if (currentDay !== dayOfMonth) {
       currentMessageNumber = 0;
-      vatsimData = []; // Clear the data for a new day
+      vatsimData = []; 
     }
 
     currentDay = dayOfMonth;
 
-    // Rebuild vatsimData from scratch
     vatsimData = restructureData(filterVatsimData(response.data));
   } catch (error) {
     console.error("Hiba történt a VATSIM adatok lekérésekor:", error.message);
@@ -41,7 +26,7 @@ async function fetchVatsimData() {
 }
 
 function restructureData(data) {
-  let newData = [];
+  let newData = vatsimData;
 
   data.forEach((element) => {
     if (element.flight_plan) {
@@ -52,39 +37,46 @@ function restructureData(data) {
       let equipment = aircraftSplit[1].split("-")[1];
       let TransponderEquipment = aircraftSplit[2];
 
-      // Check if the callsign already exists in vatsimData
-      const existingAircraft = vatsimData.find((item) => item.callsign === element.callsign);
+      const existingIndex = vatsimData.findIndex((item) => item.callsign === element.callsign);
+      const existingAircraft = vatsimData[existingIndex];
+
       const isNew = !existingAircraft;
 
       if (isNew) {
-        currentMessageNumber++; // Increment message number for new aircraft
+        currentMessageNumber++;
       }
 
       let currJson = {
-        callsign: element.callsign ? element.callsign : "unknown",
-        groundspeed: element.groundspeed ? element.groundspeed : "unknown",
-        transponderCode: element.transponder ? element.transponder : "unknown",
-        flightRules: element.flight_plan.flight_rules ? element.flight_plan.flight_rules : "unknown",
-        aircraftType: atyp,
-        aircraftWTC: wtc,
-        aircraftEquipment: equipment,
-        aircraftTransponder: TransponderEquipment,
-        departure: element.flight_plan.departure ? element.flight_plan.departure : "unknown",
-        arrival: element.flight_plan.arrival ? element.flight_plan.arrival : "unknown",
-        alternate: element.flight_plan.alternate ? element.flight_plan.alternate : "unknown",
-        cruiseTAS: element.flight_plan.cruise_tas ? element.flight_plan.cruise_tas : "unknown",
-        altitude: element.flight_plan.altitude ? element.flight_plan.altitude : "unknown",
-        departureTime: element.flight_plan.deptime ? element.flight_plan.deptime : "unknown",
-        enrouteTime: element.flight_plan.enroute_time ? element.flight_plan.enroute_time : "unknown",
-        fuelTime: element.flight_plan.fuel_time ? element.flight_plan.fuel_time : "unknown",
-        remarks: element.flight_plan.remarks ? element.flight_plan.remarks : "unknown",
-        route: element.flight_plan.route ? element.flight_plan.route : "unknown",
+        callsign: element.callsign !== undefined ? element.callsign : "unknown",
+        groundspeed: element.hasOwnProperty("groundspeed") ? element.groundspeed : "unknown",
+        transponderCode: element.hasOwnProperty("transponder") ? element.transponder : "unknown",
+        flightRules: element.flight_plan && element.flight_plan.hasOwnProperty("flight_rules") ? element.flight_plan.flight_rules : "unknown",
+        aircraftType: atyp !== undefined ? atyp : "unknown",
+        aircraftWTC: wtc !== undefined ? wtc : "unknown",
+        aircraftEquipment: equipment !== undefined ? equipment : "unknown",
+        aircraftTransponder: TransponderEquipment !== undefined ? TransponderEquipment : "unknown",
+        departure: element.flight_plan && element.flight_plan.hasOwnProperty("departure") ? element.flight_plan.departure : "unknown",
+        arrival: element.flight_plan && element.flight_plan.hasOwnProperty("arrival") ? element.flight_plan.arrival : "unknown",
+        alternate: element.flight_plan && element.flight_plan.hasOwnProperty("alternate") ? element.flight_plan.alternate : "unknown",
+        cruiseTAS: element.flight_plan && element.flight_plan.hasOwnProperty("cruise_tas") ? element.flight_plan.cruise_tas : "unknown",
+        altitude: element.flight_plan && element.flight_plan.hasOwnProperty("altitude") ? element.flight_plan.altitude : "unknown",
+        departureTime: element.flight_plan && element.flight_plan.hasOwnProperty("deptime") ? element.flight_plan.deptime : "unknown",
+        enrouteTime: element.flight_plan && element.flight_plan.hasOwnProperty("enroute_time") ? element.flight_plan.enroute_time : "unknown",
+        fuelTime: element.flight_plan && element.flight_plan.hasOwnProperty("fuel_time") ? element.flight_plan.fuel_time : "unknown",
+        remarks: element.flight_plan && element.flight_plan.hasOwnProperty("remarks") ? element.flight_plan.remarks : "unknown",
+        route: element.flight_plan && element.flight_plan.hasOwnProperty("route") ? element.flight_plan.route : "unknown",
         isNew: isNew,
         messageNumber: isNew ? currentMessageNumber : existingAircraft.messageNumber,
         inFlight: element.groundspeed >= 60,
-        fileTime: new Date().toISOString(), // Timestamp for the current data
+        fileTime: isNew ? new Date().toISOString() : existingAircraft.fileTime,
       };
-      newData.push(currJson);
+
+      if (existingAircraft) {
+        newData[existingIndex] = currJson;
+      } else {
+        newData.push(currJson);
+
+      }
     }
   });
 
@@ -92,19 +84,13 @@ function restructureData(data) {
 }
 
 function filterVatsimData(data) {
-  data = data.pilots;
+  data = [...(data.pilots || []), ...(data.prefiles || [])];
 
-  data = data.filter((item) => 
-    item.flight_plan && 
-    item.flight_plan.remarks && 
-    (item.flight_plan.remarks.includes("LHCC") || 
-    LHCC_BELEPOK.some((item2) => item.flight_plan.route.includes(item2)))
-  );
+  data = data.filter((item) => item.flight_plan && item.flight_plan.remarks && (item.flight_plan.remarks.includes("LHCC") || LHCC_BELEPOK.some((item2) => item.flight_plan.route.includes(item2)) || item.flight_plan.departure.startsWith("LH") || item.flight_plan.arrival.startsWith("LH")));
 
   return data;
 }
 
-// Fetch data initially and then every 15 seconds
 fetchVatsimData();
 setInterval(fetchVatsimData, 15000);
 
