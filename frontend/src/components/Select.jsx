8d9be6e3fd0aa45f9604d302eb3c5ec985cc20b5
Csaba44 <csabi16sheldon@gmail.com>
2025-03-28 10:38:17 +0100

    if only one option select that

--- frontend/src/components/Select.jsx
+++ frontend/src/components/Select.jsx
@@ -1,9 +1,15 @@
-import React from "react";
+import React, { useEffect } from "react";
 
 function Select({ value, onChange, options, defaultOptionLabel = "Please select", getOptionLabel = (option) => option, getOptionValue = (option) => option, className = "" }) {
+  useEffect(() => {
+    if (options.length === 1 && value !== getOptionValue(options[0])) {
+      onChange({ target: { value: getOptionValue(options[0]) } });
+    }
+  }, [options, value, onChange, getOptionValue]);
+
   return (
     <select value={value} onChange={onChange} className={"peer rounded-[7px] border border-blue-gray-200 bg-transparent px-3 py-2.5 font-sans text-sm font-normal text-blue-gray-700 outline outline-0 transition-all placeholder-shown:border placeholder-shown:border-blue-gray-200 placeholder-shown:border-t-blue-gray-200 empty:!bg-gray-900 focus:border-1 focus:border-gray-900 focus:outline-0 disabled:border-0 disabled:bg-blue-gray-50" + " " + className}>
-      <option value="none">{defaultOptionLabel}</option>
+      {options.length !== 1 && <option value="none">{defaultOptionLabel}</option>}
       {options.map((option, index) => (
         <option key={index} value={getOptionValue(option)}>
           {getOptionLabel(option)}
