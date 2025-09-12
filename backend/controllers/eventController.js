fac359ba93ecf420f00c01031e41d8b2708b8797
Csaba44 <csabi16sheldon@gmail.com>
2025-09-12 17:55:28 +0200

    event backend finished

--- backend/controllers/eventController.js
+++ backend/controllers/eventController.js
@@ -1,10 +1,8 @@
 const pool = require("../config/mysql");
 
-
-
 const getCustomEvents = async () => {
   try {
-    const [rows, fields] = await pool.query(`SELECT name, start_time, end_time, description from events WHERE start_time >= CURDATE()`);
+    const [rows, fields] = await pool.query(`SELECT * from events WHERE start_time >= CURDATE()`);
     return { events: rows, count: rows.length };
   } catch (error) {
     console.error("Database Error:", error);
@@ -12,4 +10,80 @@ const getCustomEvents = async () => {
   }
 };
 
-module.exports = {getCustomEvents}
+const getCustomEventByID = async (id) => {
+  try {
+    const query = `SELECT * from events WHERE start_time >= CURDATE() AND id = ?`;
+
+    const values = [id];
+
+    const [rows, fields] = await pool.query(query, values);
+
+    return { events: rows, count: rows.length };
+  } catch (error) {
+    console.error("Database Error:", error);
+    return { error: error };
+  }
+};
+
+const createEvent = async (name, startTime, endTime, description) => {
+  if (!name || !startTime || !endTime || !description) {
+    return { message: "Missing fields." };
+  }
+
+  try {
+    const query = `INSERT INTO events (name, start_time, end_time, description) VALUES (?, ?, ?, ?)`;
+
+    const values = [name, startTime, endTime, description];
+    const [rows, fields] = await pool.query(query, values);
+
+    return { result: rows };
+  } catch (error) {
+    console.error("Database Error:", error);
+    return { error: error };
+  }
+};
+
+const updateEvent = async (id, updates) => {
+  if (!id || Object.keys(updates).length === 0) {
+    return { message: "Missing fields." };
+  }
+
+  try {
+    let updateQuery = "UPDATE events SET ";
+
+    const updateFields = [];
+    const values = [];
+
+    Object.entries(updates).forEach(([key, value]) => {
+      updateFields.push(`${key} = ?`);
+      values.push(value);
+    });
+
+    updateQuery += updateFields.join(", ");
+    updateQuery += ` WHERE id = ?`;
+    values.push(id);
+
+    const [rows] = await pool.query(updateQuery, values);
+
+    return { result: rows };
+  } catch (error) {
+    console.error("Database Error:", error);
+    return { error: error };
+  }
+};
+
+const deleteEvent = async (id) => {
+  try {
+    const query = `DELETE FROM events WHERE id = ?`;
+
+    const values = [id];
+
+    const [rows, fields] = await pool.query(query, values);
+    return { result: { affectedRows: 1 } };
+  } catch (error) {
+    console.error("Database Error:", error);
+    return { error: error };
+  }
+};
+
+module.exports = { getCustomEvents, getCustomEventByID, createEvent, updateEvent, deleteEvent };
--- backend/routes/eventsRoute.js
+++ backend/routes/eventsRoute.js
@@ -2,16 +2,83 @@ const express = require("express");
 const router = express.Router();
 require("dotenv").config();
 const { getEvents } = require("../utils/getEvents");
-const {getCustomEvents} = require("../controllers/eventController");
+const { getCustomEvents, getCustomEventByID, createEvent, updateEvent, deleteEvent } = require("../controllers/eventController");
 
 router.get("/", async (req, res) => {
   try {
-    const events = await getEvents();  // VATSIM events
+    const events = await getEvents(); // VATSIM events
     const customEvents = await getCustomEvents();
     return res.status(200).send(events.concat(customEvents.events));
   } catch (error) {
     console.log(error.message);
     res.status(500).send({ message: error.message });
+  }
+});
+
+router.get("/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const events = await getCustomEventByID(id);
+    return res.status(200).send(events);
+  } catch (error) {
+    console.log(error.message);
+    res.status(500).send({ message: error.message });
+  }
+});
+
+router.post("/add", async (req, res) => {
+  if (!req.body.name || !req.body.start_time || !req.body.end_time || !req.body.description) {
+    return res.status(400).send({
+      error: "Send all required fields: name, start_time, end_time, description",
+    });
+  }
+  try {
+    const visitors = await createEvent(req.body.name, req.body.start_time, req.body.end_time, req.body.description);
+    return res.status(200).send(visitors);
+  } catch (error) {
+    console.log(error.message);
+    res.status(500).send({ message: error.message });
+  }
+});
+
+router.put("/update/:id", async (req, res) => {
+  const id = req.params.id;
+  const updates = req.body;
+
+  try {
+    if (!Object.keys(updates).length) {
+      return res.status(400).send({
+        error: "Send at least one field to update",
+      });
+    }
+
+    const result = await updateEvent(id, updates);
+
+    if (result.error) {
+      return res.status(500).send({ message: result.error.message });
+    }
+
+    return res.status(200).send(result.result);
+  } catch (error) {
+    console.log(error.message);
+    return res.status(500).send({ message: error.message });
+  }
+});
+
+router.delete("/delete/:id", async (req, res) => {
+  const { id } = req.params;
+
+  try {
+    const result = await deleteEvent(id);
+
+    if (result.error) {
+      return res.status(500).send({ message: result.error.message });
+    }
+
+    return res.status(200).send(result.result);
+  } catch (error) {
+    console.log(error.message);
+    return res.status(500).send({ message: error.message });
   }
 });
 
