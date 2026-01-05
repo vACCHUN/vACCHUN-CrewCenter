ae1886f2bc9eefe4223ffc78ae4a3496a0007097
Csaba44 <csabi16sheldon@gmail.com>
2026-01-05 12:21:15 +0100

    Fix: border color

--- frontend/src/AFTN/AftnMain.tsx
+++ frontend/src/AFTN/AftnMain.tsx
@@ -16,7 +16,7 @@ function Aftn() {
       <div className="grid grid-cols-2 py-3">
         <div className="w-fit px-2">
           <div className="p-1 bg-[#f5f5f5]">
-            <span className="bg-[#f0f0f0] p-1 border border-blue-500! text-lime-500 font-bold">LINE</span>
+            <span className="bg-[#f0f0f0] p-1 border border-blue-500 text-lime-500 font-bold">LINE</span>
           </div>
         </div>
         <div className="flex justify-end px-12">
