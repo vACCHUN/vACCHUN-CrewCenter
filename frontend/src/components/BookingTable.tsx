d818be5e7cbee40375c350aefa511be1570547f1
Csaba44 <csabi16sheldon@gmail.com>
2026-01-08 13:28:46 +0100

    Fix: removed unused import

--- frontend/src/components/BookingTable.tsx
+++ frontend/src/components/BookingTable.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useRef, useState } from "react";
+import { useEffect, useState } from "react";
 import CreateBookingPopup from "./CreateBookingPopup.tsx";
 import "./BookingTable.css";
 import config from "../config.ts";
